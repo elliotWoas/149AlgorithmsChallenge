74366ee1d996731501911c2d8bc3e51b756ea193
elliot sad <bamdadmahdi05@gmail.com>
2025-01-30 22:54:23 +0100

    challange 7 : almost Increasing Sequence

--- src/almostIncreasingSequence/almostIncreasingSequence.ts
+++ src/almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,20 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
+    let count = 0;
 
+    for (let i = 0; i < sequence.length - 1; i++) {
+        if (sequence[i] >= sequence[i + 1]) {
+            count++;
+            if (count > 1) {
+                return false;
+            }
+            if (i > 0 && sequence[i - 1] >= sequence[i + 1] && i + 2 < sequence.length && sequence[i] >= sequence[i + 2]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])); 
+console.log(almostIncreasingSequence([1, 3,5, 2]));    
