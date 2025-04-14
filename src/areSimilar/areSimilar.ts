71c52d8a4523f016b9871e38a5afe0c59c410678
elliot sad <bamdadmahdi05@gmail.com>
2025-04-14 23:54:53 +0200

    add 2 new algorithm challage (12)

---
+++ src/areSimilar/areSimilar.ts
@@ -0,0 +1,8 @@
+export function areSimilar(a: number[], b: number[]): boolean {
+    
+}
+
+// Example usage
+console.log(areSimilar([1, 2, 3], [1, 2, 3])); 
+console.log(areSimilar([1, 2, 3], [2, 1, 3])); 
+console.log(areSimilar([1, 2, 2], [2, 1, 1])); 
