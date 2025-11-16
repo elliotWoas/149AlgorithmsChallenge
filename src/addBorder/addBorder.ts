ffcaa848c5fae9002acc9c83ce9954bd2a77576c
keyhan <106772247+elliotWoas@users.noreply.github.com>
2025-11-16 07:43:29 +0100

    Solutions woasmit (#7)
    * remove absolute value sum .
    
    * remove add challenge  answer
    
    * remove implementation of addBorder function
    
    * remove implementation of addBorder function (#3)
    
    * remove implementation of add two digits function
    
    * remove implementation of adjacent elements product function
    
    * remove implementation of adjacent elements products function
    
    ---------
    
    Co-authored-by: dpr <mehdisadismi@gmail.com>
    Co-authored-by: bit.waos <108035654+woasmit@users.noreply.github.com>

--- src/addBorder/addBorder.ts
+++ src/addBorder/addBorder.ts
@@ -1,13 +1,5 @@
 export function addBorder(picture: string[]): string[] {
-    const wall = '*'.repeat(picture[0].length + 2);
-    const result = [wall];
 
-    for (const line of picture) {
-        result.push('*'.concat(line, '*'));
-    }
-
-    result.push(wall);
-    return result;
 }
 
 console.log(addBorder(["abc", "ded"]))
--- src/addTwoDigits/addTwoDigits.ts
+++ src/addTwoDigits/addTwoDigits.ts
@@ -1,9 +1,5 @@
 export function addTwoDigits(n: any): number {
 
-    const str = n.toString();
-    const digits = str.split('');
-    const sum = digits.reduce((acc: number, digit: string) => acc + parseInt(digit, 10), 0)
-    return sum;
     
 }
 
