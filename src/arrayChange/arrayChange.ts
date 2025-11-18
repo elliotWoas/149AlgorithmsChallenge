f834fe5c6f9d121aafe47d3c6eaa7001124839ff
amirhoseinLotfii <amirhoseinlotfalizadeprogramer@gmail.com>
2025-11-18 21:05:08 +0330

    Implement arrayChange function to calculate minimum moves for array adjustment

--- src/arrayChange/arrayChange.ts
+++ src/arrayChange/arrayChange.ts
@@ -1,5 +1,10 @@
 export function arrayChange(inputArray: number[]): number {
-
+  return inputArray.reduce((moves, curr, i) => {
+    if (i > 0 && curr <= inputArray[i - 1]) {
+      inputArray[i] = inputArray[i - 1] + 1;
+      return moves + inputArray[i] - curr;
+    }
+    return moves;
+  }, 0);
 }
 
-// console.log(arrayChange([1, 1, 1]));
