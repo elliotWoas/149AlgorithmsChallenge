a303c0ecacc17f0e68455f7541fb7091b52e9775
parsamoloody <parsaprogramerjs@gmail.com>
2025-11-21 23:54:54 +0330

    i solved the addTwoDigits with one solution

--- src/addTwoDigits/addTwoDigits.ts
+++ src/addTwoDigits/addTwoDigits.ts
@@ -1,6 +1,10 @@
 export function addTwoDigits(n: any): number {
 
-    
-}
+    return n
+        .toString()
+        .split('')
+        .map(Number)
+        .reduce((ac: number, cu: number) => ac + cu)
 
+}
 console.log(addTwoDigits(29));
