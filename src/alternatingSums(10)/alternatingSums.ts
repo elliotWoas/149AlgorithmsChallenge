dbf03d936c4149718c897aeb8eabf6af26a1f4b0
dpr <mehdisadismi@gmail.com>
2025-11-30 10:12:41 +0100

    solved alternating sums algorithms

--- src/alternatingSums(10)/alternatingSums.ts
+++ src/alternatingSums(10)/alternatingSums.ts
@@ -1,5 +1,21 @@
-export function alternatingSums(a: number[]): number[] {
+export function alternatingSums(weights: number[]): number[] {
 
+    let teamTotal1 = 0;
+    let teamTotal2 = 0;
+
+    for(let index = 0; index < weights.length; index++){
+        const personWeights = weights[index];
+        const isTeam1 = index  %2  === 0;
+
+        if(isTeam1) {
+            teamTotal1 += personWeights;
+        }else {
+            teamTotal2 += personWeights
+        }
+    }
+
+    return [teamTotal1 , teamTotal2]
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]));
+console.log(alternatingSums([50, 60, 60, 45, 70])); 
+
