4c47dc1e3c056616005101c97be6238aa372dadb
dpr <mehdisadismi@gmail.com>
2025-12-02 15:53:07 +0100

    add border to picture in addBorder function

--- src/addBorder/addBorder.ts
+++ src/addBorder/addBorder.ts
@@ -1,5 +1,17 @@
 export function addBorder(picture: string[]): string[] {
+  let borderLentgh = picture.length + 3;
+  let border = "";
 
+  for (let i = 0; i < borderLentgh; i++) {
+    border += "*";
+}
+for (let i = 0; i < picture.length; i++) {
+  picture[i] = "*" + picture[i] + "*";
 }
 
-console.log(addBorder(["abc", "ded"]))
+  picture.unshift(border);
+  picture.push(border);
+  return picture
+}
+
+console.log(addBorder(["abc", "ded"]));
