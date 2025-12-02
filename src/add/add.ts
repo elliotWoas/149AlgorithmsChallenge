f0660a24a5e3235278a2418bb7aab85e4f38b11c
mohammad esmailzadeh <esma.mohammade@gmail.com>
2025-12-02 10:06:41 +0330

    Calculate the sum of the numbers

--- src/add/add.ts
+++ src/add/add.ts
@@ -5,8 +5,8 @@ export function add(param1: number, param2: number): number {
 const sunParam=param1+param2
 return sunParam
 }
-console.log(add(1, 2));
-console.log(add(3, 2));
+// console.log(add(1, 2));
+// console.log(add(3, 2));
 
 
 //Function to sum any number of numbers entered
@@ -20,5 +20,5 @@ for (let index = 0; index < params.length; index++) {
 return sunParam
 }
 
-console.log(add2(1,2,3,6,4,5));
-console.log(add2(2,3));
+// console.log(add2(1,2,3,6,4,5));
+// console.log(add2(2,3));
