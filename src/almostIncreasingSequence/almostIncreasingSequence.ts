1929f7fa7b827d441087c2d5f51c3347bfa222d6
mohammad esmailzadeh <esma.mohammade@gmail.com>
2025-12-09 10:56:19 +0330

    Solve the almostIncreasingSequence challenge

--- src/almostIncreasingSequence/almostIncreasingSequence.ts
+++ src/almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,5 +1,18 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
+  let x = 0;
+
+  for (let index = 0; index < sequence.length - 1; index++) {
+    if (sequence[index] > sequence[index + 1]) {
+      x++;
+    }
+  }
+
+  if (x < 2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(almostIncreasingSequence([1, 3, 2, 1])); 
-console.log(almostIncreasingSequence([1, 3,5, 2]));    
+console.log(almostIncreasingSequence([1, 3, 2, 1]));
+console.log(almostIncreasingSequence([1, 3, 5, 2]));
