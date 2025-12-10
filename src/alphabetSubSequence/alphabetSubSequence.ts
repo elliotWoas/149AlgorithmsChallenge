23006cf348462390a311c18ae500a9701bc3e31c
mohammad esmailzadeh <esma.mohammade@gmail.com>
2025-12-10 16:40:35 +0330

    Solve the alphabet Subsequence challenge

--- src/alphabetSubSequence/alphabetSubSequence.ts
+++ src/alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,20 @@
 export function alphabetSubsequence(s: string): boolean {
+  let outputFunction = true;
+  const arrayString = s.split("");
 
+  for (let index = 0; index < arrayString.length - 1; index++) {
+    const firstString = arrayString[index].charCodeAt(0);
+    const secondString = arrayString[index + 1].charCodeAt(0);
+
+    if (firstString > secondString) {
+      outputFunction = false;
+    }
+  }
+  return outputFunction;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence("zab"));
+console.log(alphabetSubsequence("effg"));
+console.log(alphabetSubsequence("cdce"));
+console.log(alphabetSubsequence("ace"));
+console.log(alphabetSubsequence("bxz"));
