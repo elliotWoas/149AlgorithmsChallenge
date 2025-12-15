f784927ce8275e6e823b0dffea408acc6f6c1f92
mohammad esmailzadeh <esma.mohammade@gmail.com>
2025-12-15 10:23:25 +0330

    Solving the alternatingSums challenge

--- src/alternatingSums(10)/alternatingSums.ts
+++ src/alternatingSums(10)/alternatingSums.ts
@@ -1,5 +1,15 @@
 export function alternatingSums(a: number[]): number[] {
+  let sunFirst: number = 0;
+  let sunSecond: number = 0;
 
+  a.forEach((value, index) => {
+    if (index % 2 === 0) {
+      sunSecond+=value
+    } else {
+      sunFirst+=value;
+    }
+  });
+return [sunFirst,sunSecond]  
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]));
