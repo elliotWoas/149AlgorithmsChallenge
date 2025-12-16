08a7218ceed72859afb68dccee36c41ca97be259
mohammad esmailzadeh <esma.mohammade@gmail.com>
2025-12-16 19:25:22 +0330

    Solving the areEquallyStrong challenge

--- src/areEquallyStrong/areEquallyStrong.ts
+++ src/areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,17 @@
 export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
 
+    let outputFunction:boolean=true
+
+    if (yourLeft!=friendsLeft&&yourLeft!=friendsRight) {
+        outputFunction=false
+    }
+    if (yourRight!=friendsLeft&&yourRight!=friendsRight) {
+        outputFunction=false
+    }
+
+    return outputFunction
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
