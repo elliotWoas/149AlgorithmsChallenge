8c0d684471e367c482edceefc3cf6b32cde92f7f
khodaeidev <khodaei.dev@gmail.com>
2025-12-16 09:09:30 +0330

    khodaeiDev - Solving the add algorithm

--- src/add/add.ts
+++ src/add/add.ts
@@ -1,8 +1,20 @@
 export function add(param1: number, param2: number): number {
-
+  let sum: number = param1 + param2;
+  return sum;
 }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+export function add2(...params: number[]): number {
+  let sum: number = 0;
+
+  params.forEach((param) => {
+    sum += param;
+  });
+
+  return sum;
+}
+
+console.log(add(-100, 1000));
+console.log(add(3, 2));
+
+console.log(add2(-1, 2, -3, 4, 5));
+console.log(add2(2, 3));
