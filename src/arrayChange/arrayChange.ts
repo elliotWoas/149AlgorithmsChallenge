ae4ad82494f9259354dcbb025e236a1c0f1a86c3
mohammad esmailzadeh <esma.mohammade@gmail.com>
2025-12-23 10:39:49 +0330

    Solve the arrayChange challenge

--- src/arrayChange/arrayChange.ts
+++ src/arrayChange/arrayChange.ts
@@ -1,5 +1,13 @@
 export function arrayChange(inputArray: number[]): number {
+let sunChanges:number=0
 
-}
+  for (let index = 0; index < inputArray.length-1; index++) {
+    if (inputArray[index] >= inputArray[index + 1]) {
+        sunChanges+= inputArray[index] - inputArray[index + 1] + 1;
+      inputArray[index + 1] += inputArray[index] - inputArray[index + 1] + 1;    
+    }
+  }
+return sunChanges}
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([4, 3, 4,2]));
