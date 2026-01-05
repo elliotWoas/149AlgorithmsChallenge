b38f2bd7602df9dd17c005ee1231a39ca04d1ad6
mohammad <esma.mohammade@gmail.com>
2026-01-05 09:28:19 +0330

    Solve the avoidObstacles challenge

--- src/avoidObstacles/avoidObstacles.ts
+++ src/avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,19 @@
 export function avoidObstacles(inputArray: number[]): number {
+  inputArray.sort((a, b) => a - b);
+  let output: number;
+  for (let index = 1; index <= inputArray[inputArray.length - 1]; index++) {
+    const isTrue = inputArray.every((value) => value % index !== 0);
+    console.log(isTrue);
 
+    if (isTrue) {
+      output = index;
+      break;
+    }
+    if (index === inputArray[inputArray.length - 1] && !isTrue) {
+      output = inputArray[inputArray.length - 1] + 1;
+    }
+  }
+  return output!;
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
