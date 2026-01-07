24ff69853896dedeec794441c76818dff4ea7739
mohammad <esma.mohammade@gmail.com>
2026-01-07 11:31:04 +0330

    Solve the bishopAndPawn challenge

--- src/bishopAndPawn(20)/bishopAndPawn.ts
+++ src/bishopAndPawn(20)/bishopAndPawn.ts
@@ -1,5 +1,21 @@
-export function bishopAndPawn(bishop: string, pawn: string): boolean {
+import { Chess, Square } from "chess.js";
 
+export function bishopAndPawn(bishop: Square, pawn: string): boolean {
+  let output = false;
+  const chess = new Chess();
+  chess.clear();
+
+  chess.put({ type: "b", color: "w" }, bishop);
+
+  const moves = chess.moves({
+    square: bishop,
+  });
+  moves.forEach((item) => {
+    if (item.includes(pawn)) {
+      output = true;
+    }
+  });
+  return output;
 }
 
-// console.log(bishopAndPawn('a1', 'c3'));
+console.log(bishopAndPawn("a1", "c3"));
